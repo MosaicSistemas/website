dc23d26d4e027681660d67b4ed24552f2df7272c
MosaicBridge <igorcrissaff06@gmail.com>
2022-01-31 23:05:37 -0300

    site 4.0
    new layout

---
+++ redirects.js
@@ -0,0 +1,6 @@
+function cadastrar() {
+    id = document.getElementById('codigo')
+    nome = document.getElementById('codigo')
+    valor = document.getElementById('codigo')
+    window.location.replace = `file:///C:/Users/IGOR/Desktop/github/website/index.html/${id}/${nome}/${valor}`
+}
